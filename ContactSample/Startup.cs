4a3daab9769d223a3dc8ec5bcc8409fc2fe53d75
Misadri2 <mis.monterio@hotmail.com>
2022-05-07 20:26:49 -0300

    configuração startup

--- ContactSample/Startup.cs
+++ ContactSample/Startup.cs
@@ -1,21 +1,36 @@
+using Amazon.DynamoDBv2;
+using Amazon.SQS;
+using ContactSample.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace ContactSample
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly string _applicationName;
+
+        [System.Obsolete]
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+          var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"app.setting.{env.EnvironmentName}.json", optional: true) 
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            _applicationName = Configuration["ApplicationName"];
+
         }
 
         public IConfiguration Configuration { get; }
@@ -23,12 +38,33 @@ namespace ContactSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Add AWS Services
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+            services.AddAWSService<IAmazonDynamoDB>();
+            services.AddAWSService<IAmazonSQS>();
+
+            //Add Application Services
+            services.AddTransient<ContactQueueService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddAWSProvider(this.Configuration.GetAWSLoggingConfigSection(),
+                formatter: (logLevel, message, exception) => JsonConvert.SerializeObject(
+                     new Dictionary<string, string>
+                     {
+                        {"DateTimeUTC",DateTimeOffset.UtcNow.ToString("s")},
+                        {"logLevel", logLevel.ToString()},
+                        {"message", message?.ToString()},
+                        {"exception", Common.GetJsonFromException(exception)}
+                     }));
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation($"new instance of { _applicationName} started.");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
